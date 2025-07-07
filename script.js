b413f4b7dd1697c37c5d0ec27274b28cf736cc25
Daniel van Opstal <dvopstal05@gmail.com>
2025-07-07 14:25:52 +0200

    port to zig

--- script.js
+++ script.js
@@ -15,15 +15,84 @@
 
   let exports;
 
+  const readString = (ptr, len) => {
+    const bytes = new Uint8Array(exports.memory.buffer, ptr, len);
+    return new TextDecoder().decode(bytes);
+  };
+
   const _print = (ptr, len) => {
-    const mem = new Uint8Array(instance.exports.memory.buffer);
-    const msgBytes = mem.slice(ptr, ptr + len);
-    const msg = new TextDecoder().decode(msgBytes);
-    console.log(msg);
+    console.log(readString(ptr, len));
+  };
+
+  const shaders = [];
+  const compileShader = (srcPointer, srcLen, shaderType) => {
+    const src = readString(srcPointer, srcLen);
+    const shader = gl.createShader(shaderType);
+    if (!shader) throw new Error("Shader is null...");
+    gl.shaderSource(shader, src);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      throw new Error("Error compiling shader: " + gl.getShaderInfoLog(shader));
+    }
+
+    shaders.push(shader);
+    return shaders.length - 1;
+  };
+
+  const programs = [];
+  const createProgram = (vertexShaderIdx, fragmentShaderIdx) => {
+    const glProgram = gl.createProgram();
+
+    gl.attachShader(glProgram, shaders[vertexShaderIdx]);
+    gl.attachShader(glProgram, shaders[fragmentShaderIdx]);
+    gl.linkProgram(glProgram);
+    if (!gl.getProgramParameter(glProgram, gl.LINK_STATUS)) {
+      alert("Unable to initialize the shader program");
+      return;
+    }
+
+    gl.useProgram(glProgram);
+
+    programs.push(glProgram);
+    return programs.length - 1;
+  };
+
+  const useProgram = (programIdx) => {
+    gl.useProgram(programs[programIdx]);
+  };
+
+  const createBufferAndBind = (
+    programIdx,
+    dataPtr,
+    dataLen,
+    dataSize,
+    attPtr,
+    attLen,
+  ) => {
+    const data = new Float32Array(exports.memory.buffer, dataPtr, dataLen);
+    const attName = readString(attPtr, attLen);
+
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+    const att = gl.getAttribLocation(programs[programIdx], attName);
+    gl.vertexAttribPointer(att, dataSize, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(att);
+  };
+
+  const drawArrays = (count) => {
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, count);
   };
 
   const env = {
     _print,
+    compileShader,
+    createProgram,
+    useProgram,
+    createBufferAndBind,
+    drawArrays,
+
     clearColor: (r, g, b, a) => gl.clearColor(r, g, b, a),
     clear: (x) => gl.clear(x),
   };
@@ -35,88 +104,5 @@
   const instance = results.instance;
   exports = instance.exports;
 
-  // instance.exports.init();
-
-  const vertex = `
-    attribute vec3 a_Position;
-    attribute vec2 a_Uv;
-    varying highp vec2 v_Uv;
-    void main() {
-        gl_Position = vec4(a_Position, 1.0);
-        v_Uv = a_Uv;
-    }
-  `;
-
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  if (!vertexShader) return;
-  gl.shaderSource(vertexShader, vertex);
-  gl.compileShader(vertexShader);
-  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    alert("Error compiling shader: " + gl.getShaderInfoLog(vertexShader));
-    return;
-  }
-
-  const fragment = `
-    varying highp vec2 v_Uv;
-    void main() {
-        gl_FragColor = vec4(v_Uv, 0.0, 1.0);
-    }
-  `;
-
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  if (!fragmentShader) return;
-  gl.shaderSource(fragmentShader, fragment);
-  gl.compileShader(fragmentShader);
-  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    alert("Error compiling shader: " + gl.getShaderInfoLog(fragmentShader));
-    return;
-  }
-
-  const glProgram = gl.createProgram();
-
-  gl.attachShader(glProgram, vertexShader);
-  gl.attachShader(glProgram, fragmentShader);
-  gl.linkProgram(glProgram);
-  if (!gl.getProgramParameter(glProgram, gl.LINK_STATUS)) {
-    alert("Unable to initialize the shader program");
-    return;
-  }
-
-  gl.useProgram(glProgram);
-
-  // prettier-ignore
-  const vertices = new Float32Array([
-    -1.0, -1.0, 0.0,
-    1.0, -1.0, 0.0,
-    -1.0, 1.0, 0.0,
-    1.0, 1.0, 0.0,
-  ]);
-  // prettier-ignore
-  const colors = new Float32Array([
-    0.0, 1.0,
-    1.0, 1.0,
-    0.0, 0.0,
-    1.0, 0.0,
-  ]);
-
-  const vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-  const a_Position = gl.getAttribLocation(glProgram, "a_Position");
-  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_Position);
-
-  const trianglesColorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, trianglesColorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
-  const a_Uv = gl.getAttribLocation(glProgram, "a_Uv");
-  gl.vertexAttribPointer(a_Uv, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_Uv);
-
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
-  // console.log(r);
+  exports.init();
 })();
--- src/main.zig
+++ src/main.zig
@@ -2,28 +2,75 @@ const std = @import("std");
 
 const gl = struct {
     const COLOR_BUFFER_BIT = 16384;
+    const VERTEX_SHADER = 35633;
+    const FRAGMENT_SHADER = 35632;
+
+    extern fn compileShader(ptr: [*]const u8, len: usize, shaderType: usize) usize;
+    extern fn createProgram(vertexShaderIdx: usize, fragmentShaderIdx: usize) usize;
+    extern fn useProgram(programIdx: usize) void;
+    extern fn createBufferAndBind(programIdx: usize, dataPtr: [*]const f32, dataLen: usize, dataSize: usize, attPtr: [*]const u8, attLen: usize) void;
+    extern fn drawArrays(count: usize) void;
 
     extern fn clearColor(r: f32, g: f32, b: f32, a: f32) void;
     extern fn clear(color: usize) void;
 };
 
-extern fn _print(ptr: usize, len: usize) void;
+extern fn _print(ptr: [*]const u8, len: usize) void;
 
 fn print(comptime fmt: []const u8, args: anytype) void {
     const gpa = std.heap.wasm_allocator;
     const string = std.fmt.allocPrint(gpa, fmt, args) catch unreachable;
     defer gpa.free(string);
-    _print(@intFromPtr(string.ptr), string.len);
+    _print(string.ptr, string.len);
 }
 
 export fn init() void {
-    // const gpa = std.heap.wasm_allocator;
-    //
-    // // const alloc = gpa.allocator();
-    //
-    // const xs = gpa.alloc(u8, 256) catch unreachable;
-    // defer gpa.free(xs);
+    const vertex =
+        \\ attribute vec3 a_Position;
+        \\ attribute vec2 a_Uv;
+        \\ varying highp vec2 v_Uv;
+        \\ void main() {
+        \\     gl_Position = vec4(a_Position, 1.0);
+        \\     v_Uv = a_Uv;
+        \\ }
+    ;
+    const fragment =
+        \\ varying highp vec2 v_Uv;
+        \\ void main() {
+        \\     gl_FragColor = vec4(v_Uv, 0.0, 1.0);
+        \\ }
+    ;
 
-    gl.clearColor(0, 0, 1, 1);
+    const vertices_count = 4;
+    const vertices = [_]f32{
+        -1.0, -1.0, 0.0,
+        1.0,  -1.0, 0.0,
+        -1.0, 1.0,  0.0,
+        1.0,  1.0,  0.0,
+    };
+
+    const uvs = [_]f32{
+        0.0, 1.0,
+        1.0, 1.0,
+        0.0, 0.0,
+        1.0, 0.0,
+    };
+
+    const vertex_idx = gl.compileShader(vertex.ptr, vertex.len, gl.VERTEX_SHADER);
+
+    const fragment_idx = gl.compileShader(fragment.ptr, fragment.len, gl.FRAGMENT_SHADER);
+
+    const program = gl.createProgram(vertex_idx, fragment_idx);
+    gl.useProgram(program);
+
+    const pos = "a_Position";
+    gl.createBufferAndBind(program, &vertices, vertices.len, 3, pos.ptr, pos.len);
+
+    const uv = "a_Uv";
+    gl.createBufferAndBind(program, &uvs, uvs.len, 2, uv.ptr, uv.len);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(vertices_count);
 }
